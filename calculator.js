5c736f2b6fd62aba39f3d9778a2309b4b0e67c57
Jackson Filas <jsfilas@gmail.com>
2020-09-29 10:03:37 -0700

    added Operate function and created rules for the four methods of the Calculator object using num1, op, num2 as the arguements

--- calculator.js
+++ calculator.js
@@ -18,3 +18,20 @@ let model = {
 
 };
 
+let operate = function(num1, op, num2) {
+    let result = ""
+    if (op == "add") {
+        result = model.add(num1, num2);
+    } else if (op == "subtract") {
+        result = model.subtract(num1, num2);
+    } else if (op == "multiply") {
+        result = model.multiply(num1, num2);
+    } else if (op == "divide") {
+        result = model.divide(num1, num2);
+    } else {
+        result = "error";
+    }
+
+    return result;
+}
+
