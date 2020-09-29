eb4f4819e40d8219be7ee4e9a051e71c648c4c14
Jackson Filas <jsfilas@gmail.com>
2020-09-29 14:36:49 -0700

    made methods for each of the buttons and the math functions themselves. Created the event listeners for each button which will make the calculator operational. Also accepts flaoting point numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+let currentDisplay = document.getElementById("count").innerHTML;
+let firstNumDisplay;
+let secondNumDisplay;
+let operatorClicked;
+let dotPresent = false;
+
+
 let model = {
     add: function(a, b) {
         return a + b;
@@ -21,17 +28,133 @@ let model = {
 let operate = function(num1, op, num2) {
     let result = ""
     if (op == "add") {
-        result = model.add(num1, num2);
+        result = model.add(parseFloat(num1), parseFloat(num2));
     } else if (op == "subtract") {
-        result = model.subtract(num1, num2);
+        result = model.subtract(parseFloat(num1), parseFloat(num2));
     } else if (op == "multiply") {
-        result = model.multiply(num1, num2);
+        result = model.multiply(parseFloat(num1), parseFloat(num2));
     } else if (op == "divide") {
-        result = model.divide(num1, num2);
+        result = model.divide(parseFloat(num1), parseFloat(num2));
     } else {
         result = "error";
     }
 
     return result;
 }
+
+
+
+
+let numbers = {
+    one: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 1;
+    },
+    two: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 2;
+    },
+    three: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 3;
+    },
+    four: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 4;
+    },
+    five: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 5;
+    },
+    six: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 6;
+    },
+    seven: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 7;
+    },
+    eight: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 8;
+    },
+    nine: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 9;
+    },
+    zero: function() {
+        currentDisplay = document.getElementById("count").innerHTML += 0;
+    },
+
+    clear: function() {
+        currentDisplay = document.getElementById("count").innerHTML = '';
+        dotPresent = false;
+    },
+
+    dot: function() {
+        if(dotPresent == false) {
+            currentDisplay = document.getElementById("count").innerHTML += '.';
+            dotPresent = true;
+        }
+       
+    },
+    
+};
+
+
+let work = {
+
+    add: function() {
+        firstNumDisplay = currentDisplay;
+        currentDisplay = document.getElementById("count").innerHTML = '';
+        operatorClicked ="add";
+        dotPresent = false;
+    },
+
+    subtract: function() {
+        firstNumDisplay = currentDisplay;
+        currentDisplay = document.getElementById("count").innerHTML = '';
+        operatorClicked ="subtract";
+        dotPresent = false;
+
+    },
+
+    multiply: function() {
+        firstNumDisplay = currentDisplay;
+        currentDisplay = document.getElementById("count").innerHTML = '';
+        operatorClicked = "multiply";
+        dotPresent = false;
+
+    },
+
+    divide: function() {
+        firstNumDisplay = currentDisplay;
+        currentDisplay = document.getElementById("count").innerHTML = '';
+        operatorClicked = "divide";
+        dotPresent = false;
+
+    },
+
+};
+
+let equals = function() {
+    secondNumDisplay = currentDisplay;
+    currentDisplay = document.getElementById("count").innerHTML = operate(firstNumDisplay, operatorClicked, secondNumDisplay);
+} 
+
+
+document.getElementById("one").addEventListener("click", numbers.one);
+document.getElementById("two").addEventListener("click", numbers.two);
+document.getElementById("three").addEventListener("click", numbers.three);
+document.getElementById("four").addEventListener("click", numbers.four);
+document.getElementById("five").addEventListener("click", numbers.five);
+document.getElementById("six").addEventListener("click", numbers.six);
+document.getElementById("seven").addEventListener("click", numbers.seven);
+document.getElementById("eight").addEventListener("click", numbers.eight);
+document.getElementById("nine").addEventListener("click", numbers.nine);
+document.getElementById("zero").addEventListener("click", numbers.zero);
+
+document.getElementById("clearButton").addEventListener("click", numbers.clear);
+document.getElementById("period").addEventListener("click", numbers.dot);
+
+
+document.getElementById("add").addEventListener("click", work.add);
+document.getElementById("subtract").addEventListener("click", work.subtract);
+document.getElementById("multiply").addEventListener("click", work.multiply);
+document.getElementById("divide").addEventListener("click", work.divide);
+
+document.getElementById("equals").addEventListener("click", equals);
+
+
 
